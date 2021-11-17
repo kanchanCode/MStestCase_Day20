6260a82fcacf76d4d82d838a45e4e80f565305c5
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-11-17 08:49:26 +0530

    added Code for test cases

---
+++ UC1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+    public class UC1
+    {
+        private string message;
+        public UC1()
+        {
+            message="I am in Happy Mood";
+        }
+        public UC1(string message)
+        {
+            this.message = message;
+        }
+
+        public string Analyze()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+
+            
+
+
+        }
+
+        public string ForDefault()
+        {
+            if (this.message.Contains("Happy"))
+
+                return "SAD";
+            else
+                return "HAPPY";
+            
+        }
+    }
+}
